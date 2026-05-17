d30e863cec6bfb279b965579ca05f13bbe24197a
Aman-Kasa <a.kasa@alustudent.com>
2026-05-17 02:17:06 +0200

    betty style code fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,7 +5,7 @@
  * @parent: A pointer to the node to insert the left-child in.
  * @value: The value to store in the new node.
  *
- * Return: A pointer to the created node, or NULL on failure or if parentisNULL.
+ * Return:A pointer tothecreated node, or NULL on failure or if parentis NULL.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
